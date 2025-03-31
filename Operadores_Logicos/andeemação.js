71e39cc84f784e779ddc16925547a916eb8b7281
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-03-31 08:05:17 -0300

    feat(operadores-logicos): adiciona operação AND com booleanos

---
+++ Operadores_Logicos/andeemação.js
@@ -0,0 +1,8 @@
+let neymarHabilidade = false;
+let neymarQueda = true;
+
+if (neymarHabilidade == false && neymarQueda == true ) {
+    console.log("HAHAHAHAHAHAHAHAH!");
+} else {
+    console.log("Só torce pro Santos quem é velho e gosta do Neymar. 😒");
+}
