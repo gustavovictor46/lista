3e96ee4685db6c143513220753fb6c768a83cbc0
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-03-31 09:08:58 -0300

    feat(operadores-logicos): adiciona checagem de idade pra votar

--- Operadores_Logicos/podevotar.js
+++ Operadores_Logicos/podevotar.js
@@ -1,7 +1,7 @@
-let idade = 19
+let idade = 19;
 
 if (idade >= 16 && idade <= 70) {
-    console.log("Pode votar!")
+    console.log("Pode votar!");
 } else {
-    console.log("Não pode votar!")
+    console.log("Não pode votar!");
 }
