14f5212eabbf1730a7ec1f97ea66c083992e6cbb
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-04-01 21:52:58 -0300

    feat(if-else): adiciona verificação de aprovação por nota

---
+++ If_Else/aprovado.js
@@ -0,0 +1,9 @@
+let nota = 101
+
+if (nota >=60 && nota <=100) {
+    console.log("Aprovado!");
+} else if (nota <=59) {
+    console.log("Reprovado!😢");
+} else {
+    console.log("Não dá para tirar mais de 100, né amigão?");
+}
