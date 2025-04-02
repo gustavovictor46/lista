5eb8afb7503c9c34c5d5236f8dd8822f00af363b
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-04-01 21:59:15 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/classind.js
@@ -0,0 +1,11 @@
+let idade = 54
+
+if (idade >= 0 && idade <= 12) {
+    console.log("Criança 👶");
+} else if (idade >= 13 && idade <= 17) {
+    console.log ("Adolescente 🧒");
+} else if (idade >= 18 && idade <= 59) {
+    console.log("Adulto 👨");
+} else {
+    console.log("Idoso 👴");
+}
