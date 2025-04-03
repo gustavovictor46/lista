4a3b60ddfc12c72ad2ab1644ce2d3e7039267f35
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-04-03 20:53:24 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/conceitodenota.js
@@ -0,0 +1,20 @@
+let nota = 1
+switch(nota){
+    case 1:
+        console.log("A (9-10)")
+        break;
+    case 2:
+        console.log("B (7-8)")
+        break;
+    case 3:
+        console.log("C (5-6)")
+        break;
+    case 4:
+        console.log("D (3-4)")
+        break;
+    case 5:
+        console.log("E (0-2)")
+        break;
+    default:
+        console.log("Não fez a prova")                    
+}
