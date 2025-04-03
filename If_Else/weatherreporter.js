bc9207e81aeb7887c9da14d14631ef299a4c7a4d
Gustavo Victor Ferreira <ferreirag930@yahoo.com>
2025-04-03 19:53:12 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_Else/weatherreporter.js
@@ -0,0 +1,9 @@
+let temperatura = 32;
+
+if (temperatura > 25) {
+    console.log("Tá quente demais!");
+} else if (temperatura >= 15 && temperatura <= 25) {
+    console.log("Climinha tá bom!");
+} else {
+    console.log("Que frio!");
+}
