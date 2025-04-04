d0371188f1e2e53f74f51f591d7268a422bb1bba
Gustavo Victor <ferreirag930@yahoo.com>
2025-04-04 08:00:12 -0300

    feat(if-else): adiciona cálculo de desconto baseado em salário

--- If_Else/descontinhoesperto.js
+++ If_Else/descontinhoesperto.js
@@ -1,11 +1,7 @@
-let salário = 2030;
-let valorDescontado1 = (salário * desconto1);
-let valorDescontado2 = (salário * desconto2);
-let desconto1 = 0.10;
-let desconto2 = 0.05;
+let salário = 1000;
 
 if (salário >2000) {
-    console.log(`O valor a ser descontado é de ${valorDescontado1}reais`);
+    console.log(`O valor após o desconto é de ${salário * 0.9} reais.`);
 } else{
-    console.log(`O valor a ser descontado é de ${salário * 0.05 - salário} reais`)
+    console.log(`O valor após o desconto é de ${salário * 0.95} reais.`)
 }
